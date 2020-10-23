84b2c3947b85509c059dce11ae857c4888181dc0
HectorPalacio <palaciorivas@gmail.com>
2020-10-23 11:50:40 -0500

    Conexión del FrontEnd con BackEnd

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:votaciones_app/services/socket_service.dart';
 import 'package:votaciones_app/src/pages/home.dart';
+import 'package:votaciones_app/src/pages/status.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      initialRoute: 'home',
-      routes: {'home': (context) => HomePage()},
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => SocketService()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        initialRoute: 'home',
+        routes: {
+          'home': (context) => HomePage(),
+          'status': (context) => StatusPage(),
+        },
+      ),
     );
   }
 }
